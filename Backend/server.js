3df9da79bc3192c2c6cb923fc662f7fe16c64887
Ashu <ashukr2218@gmail.com>
2024-02-08 09:31:52 +0530

    1.basic backend setup 2.mongoDB connection added 3.signup function implemented

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./Routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
 dotenv.config();
-
 const port = process.env.PORT||5000;
+app.use(express.json());// to parse the incoming requests with JSON payloads (from req.body)
+//sequence matters with this one.
+app.use("/api/auth", authRoutes);
+ 
+
 app.get("/",(req,res)=>
 {
     res.send("Hello World")
 })
+
 app.listen(port,()=>
 {
+    connectToMongoDB();
     console.log(`App is listening on port ${port}`)
 })
