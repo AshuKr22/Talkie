9a2d8886e59490a50cbbde52ef5ff1d09455e4bf
Ashu <ashukr2218@gmail.com>
2024-02-15 21:43:42 +0530

    trying deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import authRoutes from "./Routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -8,19 +9,25 @@ import userRoutes from "./Routes/user.routes.js"
 import { app,server,io } from "./socket/socket.js";
 dotenv.config();
 const port = process.env.PORT||5000;
-
+const __dirname = path.resolve();
 app.use(cookieParser());
 app.use(express.json());// to parse the incoming requests with JSON payloads (from req.body)
 //sequence matters with this one.
 app.use("/api/auth", authRoutes);
 app.use("/api/message",messageRoutes);
 app.use("/api/users",userRoutes);
- 
 
-app.get("/",(req,res)=>
-{
-    res.send("Hello World")
-})
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+ app.get("*", (req, res) => {
+ 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+ });
+
+
+// app.get("/",(req,res)=>
+// {
+//     res.send("Hello World")
+// })
 
 server.listen(port,()=>
 {
