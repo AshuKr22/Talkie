db63735f2d4e8223c2ee06fd4ef8584719518054
Ashu <ashukr2218@gmail.com>
2024-02-15 22:39:37 +0530

    test 3 : removed dev_env

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -12,7 +12,7 @@ const generateTokenandSetCookie=(userId,res)=>
         maxAge: 15*24*60*60*1000,
         httpOnly:true,
         sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development",//checks the security of cookie.
+		//checks the security of cookie.
 
     }
     );
